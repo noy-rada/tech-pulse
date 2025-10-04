e6ae4dd4cacfca89a0e53fea2ab489f74f03a384
noyrada <noy.rada.unique@gmail.com>
2025-10-05 04:20:40 +0700

    Initialize project platform of Technology

---
+++ src/main/java/techpulse/TechPulseApplication.java
@@ -0,0 +1,13 @@
+package techpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechPulseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechPulseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/techpulse/TechPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package techpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechPulseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
